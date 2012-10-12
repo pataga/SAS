ec9e354390e94252051f5e27c911b8e55d322ab8
Tanja <tanja@tanja-N55SF.(none)>
2012-10-12 15:17:17 +0200

    Füge Script für Usererstellung hinzu

--- module/servercmd/adduser.php
+++ module/servercmd/adduser.php
@@ -1,6 +1,11 @@
 <?php
     include '../../main/conf/config.php';
     
-    //   $password = md5($_POST['password']);
-    mysql_query("INSERT INTO sas_web_users (username, password) VALUES ('$username','$password'");
+    if (isset($_POST['user']) && isset($_POST['pass']))
+    {
+        $password = md5($_POST['pass']);
+        $username = ($_POST['user']);
+        mysql_query("INSERT INTO sas_web_users(username,password) values('$username', '$password')") or die(mysql_error());
+    }
+            
 ?>
