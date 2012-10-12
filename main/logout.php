6d851245ada32c26792a75d23ef715033ef4093b
Tanja <tanja@tanja-N55SF.(none)>
2012-10-12 13:36:39 +0200

    TestCommit

--- main/logout.php
+++ main/logout.php
@@ -2,7 +2,7 @@
     session_start();
     unset($_SESSION['loggedin']);
     unset($_SESSION['username']);
-    unset($_SESSION['id']);
+    unset($_SESSION['id']); //test
     session_destroy();
     echo '<meta http-equiv="refresh" content="0; URL=login/">';
 ?>
