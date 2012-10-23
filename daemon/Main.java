136dbc88dbc3c43a483490b49b49f18eccbb1b34
patrick <patrick@pc.(none)>
2012-10-23 17:50:25 +0200

    Daemon/Main: * Richte Thread zum Abhören des Ports 9100 ein * Das Programm ist nun in der Lage Nachrichten zu senden und zu empfangen * Erstelle Testprogramm in PHP, dass sich mit unserem Daemon verbindet

--- daemon/Main.java
+++ daemon/Main.java
@@ -1,7 +1,44 @@
-public class Main
-{
-	public static void main(String[] args)
-	{
-		
-	}
+
+import java.io.*;
+import java.net.*;
+
+public class Main {
+
+    protected void start() {
+        ServerSocket s;
+
+        try {
+            s = new ServerSocket(9100);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            return;
+        }
+
+        System.out.println("Warte auf Verbindung...");
+
+        for (;;) {
+            try {
+                Socket remote = s.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        remote.getInputStream()));
+                PrintWriter out = new PrintWriter(remote.getOutputStream());
+
+                String str = ".";
+                while (!str.equals("")) {
+                    str = in.readLine();
+                    System.out.println(str);
+                }
+                out.println("Hallo Du");
+                out.flush();
+                remote.close();
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Main thread = new Main();
+        thread.start();
+    }
 }
