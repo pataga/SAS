1d3d537456f3731de58fdffcaa828d138a63e593
patrick <patrick@pc.(none)>
2012-11-06 20:14:48 +0100

    Auth: Typo

--- main/login/login.php
+++ main/login/login.php
@@ -2,7 +2,7 @@
 session_start();
 
 include '../config/config.mysql.php';
-include '../functions/class.auth.php';
+include '../functions/func.auth.php';
 
 if (isset($_POST['user']) && isset($_POST['pass']))
     $_SESSION['loggedin'] = AuthChallenge($_POST['user'], $_POST['pass']);
