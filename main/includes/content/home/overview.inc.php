6cbfd9dab163f2b706b3e6520b616a2a1ef2d3a0
GabrielWanzek <gabrielwanzek@ymail.com>
2012-11-22 17:56:52 +0100

    Serverauswahl-System
    Sowie eine CSS Klasse für <select>
    
    Hinweis: Formulare müssen im <fieldset> stehen!

--- main/includes/content/home/overview.inc.php
+++ main/includes/content/home/overview.inc.php
@@ -1,6 +1,17 @@
-<h3>Standard CSS-Forms</h3>
+<h3>Serverauswahl</h3>
 <fieldset>
-<p>Pfad zur Datei mit Inhalt: 
-<br><br>
-    <?php echo __file__;?></p>
+	<p>Bitte w&auml;hlen Sie ihren Server aus, den Sie mit SAS verwalten m&ouml;chten.</p>
+	<label>Server:</label>
+	<select>
+		<option value=""> </option> <?// leere option drin lassen \\?>
+		<option value="">Server "Melone" - 46.38.238.216</option>
+		<option value="">Server "Papaya" - 122.215.215.12</option>
+		<option value="">Server "Apfel" - 198.232.141.131</option>
+		<option value="">Server "Cherry" - 221.123.132.132</option>
+		<option value="">Server "Grapefruit" - 241.103.102.130</option>
+	</select>
+	<br><br>
+	<input class="button black"type="submit" value="Server ausw&auml;hlen">
+	<hr>
+    <b>Dev-Info:</b>&nbsp;"<?php echo __file__;?>"</p>
 </fieldset>
