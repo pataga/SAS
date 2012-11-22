62d645814d227dcdcb1811d2e429e383529f6afa
GabrielWanzek <gabrielwanzek@ymail.com>
2012-11-22 22:00:57 +0100

    Demo-Startseite
    - Serverdaten (demo)
    - Notizbuch
    
    Signed-off-by: GabrielWanzek <gabrielwanzek@ymail.com>

--- main/includes/content/home/overview.inc.php
+++ main/includes/content/home/overview.inc.php
@@ -5,31 +5,31 @@
 	 <table cellpadding="0" cellspacing="0">
                         <tr>
                             <td>Host-IP:</td>
-                            <td></td>
+                            <td>46.38.238.216</td>
                         </tr>
                         <tr class="odd">
 							<td>Host-Name:</td>
-							<td></td>
+							<td>Melone</td>
                         </tr>
                         <tr>
 							<td>Kernel Version:</td>
-							<td></td>
+							<td>Linux version 3.0.42-vs2.3.2.4.0-nc (root@build-squeeze.netcup.net) (gcc version 4.4.5 (Debian 4.4.5-8) ) </td>
                         </tr>
                         <tr class="odd">
                             <td>Distribution:</td>
-							<td></td>
+							<td>Ubuntu Server 12.04</td>
                         </tr>
                         <tr>
                             <td>Server online seit:</td>
-							<td></td>
+							<td>4 Tagen, 2 Stunden, 30 Minuten</td>
                         </tr>
                         <tr>
                             <td>Letzter Bootvorgang:</td>
-							<td></td>
+							<td>11.11.12 - 21:52:11</td>
                         </tr>
                         <tr>
                             <td>Eingeloggte User:</td>
-							<td></td>
+							<td>5</td>
                         </tr>
                         <tr>
                             <td>Load: [<a href="#" class="tooltip">Info
@@ -41,11 +41,7 @@
                             	</span>
 							</a>]
 						</td>
-							<td></td>
-                        </tr>
-                        <tr>
-                            <td>Server online seit:</td>
-							<td></td>
+							<td>0.80, 0.83, 0.73</td>
                         </tr>
                     </table>
                 </div>
@@ -53,47 +49,39 @@
                 		 <table cellpadding="0" cellspacing="0">
                         <tr>
                             <td>Apache 2:</td>
-                            <td></td>
+                            <td><span class="aktiv">Aktiv</span></td>
                         </tr>
                         <tr class="odd">
-							<td>MySQL-Server:</td>
-							<td></td>
-                        </tr>
-                        <tr>
 							<td>Postfix:</td>
-							<td></td>
+							<td><span class="inaktiv">Inaktiv</span></td>
+                        </tr>
+                        <tr>
+							<td>FTP:</td>
+							<td><span class="aktiv">Aktiv</span></td>
                         </tr>
                         <tr class="odd">
-                            <td>FTP:</td>
-							<td></td>
+                            <td>MySQL:</td>
+							<td><span class="aktiv">Aktiv</span></td>
                         </tr>
                         <tr>
-                            <td>Server online seit:</td>
-							<td></td>
+                            <td>Samba:</td>
+							<td><span class="inaktiv">Inaktiv</span></td>
                         </tr>
                         <tr>
-                            <td>Letzter Bootvorgang:</td>
-							<td></td>
+                            <td>Backups:</td>
+							<td><span class="inaktiv">Inaktiv</span></td>
                         </tr>
                         <tr>
-                            <td>Eingeloggte User:</td>
-							<td></td>
+                            <td>E-Mail-Reports:</td>
+							<td><span class="aktiv">Aktiv</span></td>
                         </tr>
                         <tr>
-                            <td>Load: [<a href="#" class="tooltip">Info
-                            	<span>
-                            		Load = "Auslastung des Servers"<br>
-                            		1. Wert: letzte Minute<br>
-                            		2. Wert: letzten 5 Minuten<br>
-                            		2. Wert: letzten 15 Minuten<br>
-                            	</span>
-							</a>]
-						</td>
-							<td></td>
+                            <td>XYZ:</td>
+							<td><span class="aktiv">Aktiv</span></td>
                         </tr>
                         <tr>
-                            <td>Server online seit:</td>
-							<td></td>
+                            <td>123:</td>
+							<td><span class="aktiv">Aktiv</span></td>
                         </tr>
                     </table>
                 </div>
