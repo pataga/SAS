3bdd27036690579a2cc9fcfe1609e5519c9c5e06
Patrick Farnkopf <patrickfarnkopf@web.de>
2012-11-29 15:57:29 +0100

    Classes/MySQL: Erweitere neue MySQL Klasse

--- main/includes/classes/class.mysql2.php
+++ main/includes/classes/class.mysql2.php
@@ -7,6 +7,7 @@ class MySQL {
     private $mysql_user;
     private $mysql_pass;
     private $mysql_db;
+    private $result;
 
     function __construct($host, $port, $user, $pass) {
         $this->mysql_host = $host;
@@ -19,9 +20,46 @@ class MySQL {
         }
     }
 
+    function __clone() {
+
+    }
+
     public function selectDB($db) {
         if (!mysql_select_db($db)) {
             throw new Exeption("Fehler beim Verbinden der Datenbank ". mysql_error());
+        }
+    }
+
+    public function Query($query) {
+        if (!($result = mysql_query($query))) {
+            throw new Exeption("Fehler beim Ausf&uuml;hren des Querys ". mysql_error());
+        } else {
+            $this->result = $result;
+            return clone $this;
+        }
+    }
+
+    public function fetchAssoc() {
+        if (!is_resource($this->result)) {
+            throw new Exeption("Result ist keine Resource ".mysql_error());
+        } else {
+            return mysql_fetch_assoc($this->result);
+        }
+    }
+
+    public function fetchArray() {
+        if (!is_resource($this->result)) {
+            throw new Exeption("Result ist keine Resource ".mysql_error());
+        } else {
+            return mysql_fetch_array($this->result);
+        }
+    }
+
+    public function fetchObject() {
+        if (!is_resource($this->result)) {
+            throw new Exeption("Result ist keine Resource ".mysql_error());
+        } else {
+            return mysql_fetch_object($this->result);
         }
     }
 }
