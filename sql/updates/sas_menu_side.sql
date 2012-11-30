692882bb4fd898918173b56a634aeb4d29e03e14
TanjaWeiser <taramowei@web.de>
2012-11-30 15:28:02 +0100

    Neue Menüeinträge für Webuser

---
+++ sql/updates/sas_menu_side.sql
@@ -0,0 +1,2 @@
+INSERT INTO sas_menu_side (name, page, spage) VALUES ('Benutzer hinzuf&uuml;gen', 'webuser', 'add');
+INSERT INTO sas_menu_side (name, page, spage) VALUES ('Benutzer bearbeiten', 'webuser', 'edit');
