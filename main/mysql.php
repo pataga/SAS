74ec01ff7df9ee5d07268dad3755050c092f33ad
Patrick Farnkopf <patrickfarnkopf@web.de>
2012-12-01 11:02:58 +0100

    Examples/MySQL: Erweitere MySQL Beispiel

--- main/mysql.php
+++ main/mysql.php
@@ -2,10 +2,11 @@
 /**
  * MySQL Klassen Test 
  */
-
+require_once 'includes/classes/class.table.php';
 require_once 'includes/classes/class.mysql.php';
-$mysql = new MySQL('127.0.0.1', '3306', 'root', 'toor');
-$mysql->selectDB('sas');
+
+$mysql = new MySQL('127.0.0.1', '3306', 'root', '123');
+$mysql->selectDB('sas2');
 
 
 $res = $mysql->Query("SELECT * FROM sas_users");
@@ -14,5 +15,16 @@ while ($row = $res->fetchArray())
 {
 	echo $row['username']."<br>";
 }
+
+$table = $mysql->tableAction('sas_users');
+
+$data = array(
+'id' => '10',
+'username' => 'test',
+'password' => md5('geheim'),
+'email' => 'test@test.de',
+);
+
+$table->insert($data);
 
 ?>
