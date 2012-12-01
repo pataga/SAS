2e6310b314c06fc77b848e2d0ee9ce3532565077
Patrick Farnkopf <patrickfarnkopf@web.de>
2012-12-01 11:42:46 +0100

    Classes/Database: Langsam aber sicher Database Klasse outsourcen

--- main/includes/classes/class.db.php
+++ main/includes/classes/class.db.php
@@ -10,38 +10,6 @@ class Database {
         $this->server = $server;
     }
 
-    public function getMySQLDatabases() {
-        $result = $this->mysql->Query("SHOW DATABASES");
-        $database = array();
-
-        for ($itr = 0; ($row = $result->fetchObject()); $itr++)
-            $database[$itr] = $row->Database;
-
-        return $database;
-    }
-
-    public function getMySQLTables($db) {
-        $result = $this->mysql->Query("SHOW TABLES FROM $db");
-        $tables = array();
-        for ($itr = 0; ($row = $result->fetchArray()); $itr++)
-            $tables[$itr] = $row[0];
-
-        return $tables;
-    }
-
-    public function getMySQLColumns($db, $table) {
-        if ($this->mysql->selectDB($db)) {
-            $result = $this->mysql->Query("SHOW COLUMNS FROM $table");
-            $colums = array();
-            for ($i = 0; $row = $result->fetchArray(); $i++) {
-                $colums[$i][0] = $row[0];
-                $colums[$i][1] = $row[1];
-            }
-            return $colums;
-        } else
-            return 0;
-    }
-
     public function addServer($name, $host, $port, $user, $pass) {
         $this->mysql->Query("INSERT INTO sas_server_data (host,port,user,pass,name) VALUES ('$host','$port','$user','$pass','$name')");
     }
