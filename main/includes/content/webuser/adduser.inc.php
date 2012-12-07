3e158b8a62cda3857548996da3228cf140f7552c
Tanja Weiser <root@tanja-N55SF.(none)>
2012-12-07 12:20:37 +0100

    kleine Änderungen

--- main/includes/content/webuser/adduser.inc.php
+++ main/includes/content/webuser/adduser.inc.php
@@ -7,26 +7,26 @@ if (isset($_POST['adduser']))
 	$repeatpasswd = $_POST['repeatpasswd'];
 }
 
+ //$user->addUser($newuser, $ppasswd $repeatpasswd, $usermail);
+
 
 ?>
 
 <!--############ User Hinzufügen ############//-->
-
+<h3>Benutzer hinzuf&uuml;gen</h3>
 <fieldset>
 <form action="index.php?p=webuser&s=add" method="POST">
-
-<h3>Benutzer hinzuf&uuml;gen</h3><br><br>
  <div class ="viertel-box"> 
- 	Username:<br><br><br>
+ 	Username:<br><br>
  	E-Mail:<br><br>
- 	Passwort:<br><br><br>
+ 	Passwort:<br><br>
  	Passwort wiederholen:<br>
  </div>
  <div class ="dreiviertel-box lastbox">	
-	<input type="text-long" name="newuser" id=""><br><br>
-	<input type="text-long" name="usermail" id=""><br><br>
-	<input type="text-long" name="passwd" id=""><br><br>
-	<input type="text-long" name="repeatpasswd" id=""><br><br><br>
+	<input type="text" class = "text-long" name="newuser" id=""><br><br>
+	<input type="text" class = "text-long" name="usermail" id=""><br><br>
+	<input type="password" class = "text-long" name="passwd" id=""><br><br>
+	<input type="password" class = "text-long" name="repeatpasswd" id=""><br><br><br>
 </div>
 	<input type="submit" class="button black" name="adduser" value="Hinzuf&uuml;gen">
 	<br><br>
--- main/includes/content/webuser/edituser.inc.php
+++ main/includes/content/webuser/edituser.inc.php
@@ -7,13 +7,15 @@ if (isset($_POST['edituser']))
 ?>
 
 <!--############ User bearbeiten ############//-->
-
+<h3>Benutzer bearbeiten</h3>
 <fieldset>
 <form action="index.php?p=webuser&s=edit" method="POST">
-
-<h3>Benutzer bearbeiten</h3><br><br>
- 	Username: <input type="text-long" name="username" id=""><br><br><br>
+<div class ="fuenftel-box"> 	
+	<br>Username: 
+</div>
+<div class="vierfuenftel-box lastbox">
+	<br><input type="text" class="text-long" name="username" id=""><br><br><br>
+</div>
 	<input type="submit" class="button black" name="edituser" value="Anzeigen">
-	<br><br>
 </form>
 </fieldset>
