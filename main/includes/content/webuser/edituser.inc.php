e25ed332cee0a706e5337ed4c1d3fd97afc1b9f3
Tanja Weiser <root@tanja-N55SF.(none)>
2012-12-07 12:22:21 +0100

    kleine Änderungen

--- main/includes/content/webuser/edituser.inc.php
+++ main/includes/content/webuser/edituser.inc.php
@@ -10,12 +10,15 @@ if (isset($_POST['edituser']))
 <h3>Benutzer bearbeiten</h3>
 <fieldset>
 <form action="index.php?p=webuser&s=edit" method="POST">
+
 <div class ="fuenftel-box"> 	
 	<br>Username: 
 </div>
+
 <div class="vierfuenftel-box lastbox">
 	<br><input type="text" class="text-long" name="username" id=""><br><br><br>
 </div>
+
 	<input type="submit" class="button black" name="edituser" value="Anzeigen">
 </form>
 </fieldset>
