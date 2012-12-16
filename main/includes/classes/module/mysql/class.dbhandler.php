047883744755ab4a31883f6685b52ba79ead0999
Patrick Farnkopf <patrickfarnkopf@web.de>
2012-12-16 14:16:42 +0100

    Module/MySQL: Cleanup

--- main/includes/classes/module/mysql/class.dbhandler.php
+++ main/includes/classes/module/mysql/class.dbhandler.php
@@ -23,24 +23,18 @@ class DBHandler {
     }
 
 
-    private function logError($error = null) {
-        $this->errorLog .= $error;
-    }
-
-
-    public function getError() {
-        return $this->errorLog;
-    }
-
-
     private function createDatabaseLink() {
         $data = $this->main->getServerInstance()->getMySQLData();
-        if (!is_array($data)) {
-            $this->logError('Fehler in DBHandler::createDatabaseLink() - Variable data ist kein Array<br>');
-        } else {
-            if (!($this->mysql = new MySQL($this->main,$data[0],$data[1],$data[2],$data[3]))) {
-                $this->logError('Fehler in DBHandler::createDatabaseLink() - Verbindungsinformationen nicht korrekt<br>');
+        try {
+            if (!is_array($data)) {
+                throw new Exception('Fehler in DBHandler::createDatabaseLink() - Variable data ist kein Array');
+            } else {
+                if (!($this->mysql = new MySQL($this->main,$data[0],$data[1],$data[2],$data[3]))) {
+                    throw new Exception('Fehler in DBHandler::createDatabaseLink() - Verbindungsinformationen nicht korrekt');
+                }
             }
+        } catch (MySQLException $e) {
+            $this->main->getDebugInstance()->error($e);
         }
     }
 
@@ -57,22 +51,22 @@ class DBHandler {
 
 
     public function getDatabases() {
-        return $this->mysql->Query("SHOW DATABASES");
+        return $this->mysql->Query('SHOW DATABASES');
     }
 
 
     public function getTables() {
-        return $this->mysql->Query("SHOW TABLES FROM ".$this->database);
+        return $this->mysql->Query('SHOW TABLES FROM '.$this->database);
     }
 
 
     public function getColumns() {
-        return $this->mysql->Query("SHOW FIELDS FROM $this->table");
+        return $this->mysql->Query('SHOW FIELDS FROM '.$this->table);
     }
 
 
     public function getTableContent() {
-        return $this->mysql->Query("SELECT * FROM $this->table");
+        return $this->mysql->Query('SELECT * FROM '.$this->table);
     }
 
 
@@ -91,44 +85,45 @@ class DBHandler {
 
     public function buildDataTable() {
         $res = $this->getColumns();
-        $dataTable = "<tr>";
+        $dataTable = '<tr>';
         $counter = 0;
         while ($col = $res->fetchArray()) {
-            $dataTable .= "<td><a href='#' class='tooltip2'><h5>$col[0]</h5><span><b>Typ</b> $col[1]</span></a></td>";
+            $dataTable .= sprintf('<td><a href="#" class="tooltip2"><h5>%s</h5><span><b>Typ</b>%s</span></a></td>',
+                          $col[0],$col[1]);
             $counter++;
         }
-        $dataTable .= "</tr>";
+        $dataTable .= '</tr>';
         $res = $this->getTableContent();
         while ($d = $res->fetchArray()) {
-            $dataTable .= "<tr>";
+            $dataTable .= '<tr>';
             for ($i=0;$i<$counter;$i++) {
-                $dataTable .= "<td>$d[$i]</td>";
+                $dataTable .= sprintf('<td>%s</td>',$d[$i]);
             }
-            $dataTable .= "</tr>";
+            $dataTable .= '</tr>';
         }
         return $dataTable;
     }
 
 
     public function DatabaseToTable() {
-        $content = "";
+        $content = '';
         $res = $this->getDatabases();
         while ($row = $res->fetchArray()) {
-            $content .= "<tr>";
-            $content .= "<td><a href='?p=mysql&s=db&db=$row[0]'> $row[0] </a></td>";
-            $content .= "</tr>";
+            $content .= '<tr>';
+            $content .= sprintf('<td><a href="?p=mysql&s=db&db=%s">%s</a></td>',$row[0],$row[0]);
+            $content .= '</tr>';
         }
         return $content;
     }
 
 
     public function TableToTable() {
-        $content = "";
+        $content = '';
         $res = $this->getTables();
         while ($row = $res->fetchArray()) {
-            $content .= "<tr>";
-            $content .= "<td><a href='?p=mysql&s=db&db=$this->database&t=$row[0]'> $row[0] </td>";
-            $content .= "</tr>";
+            $content .= '<tr>';
+            $content .= sprintf('<td><a href="?p=mysql&s=db&db=%s&t=%s"> %s </td>',$this->database,$row[0],$row[0]);
+            $content .= '</tr>';
         }
         return $content;
     }
