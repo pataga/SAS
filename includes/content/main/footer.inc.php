d20a2293fa21806b5d9811d6b1ee7b30c31194cd
Patrick Farnkopf <patrickfarnkopf@web.de>
2012-12-18 19:24:14 +0100

    Version => 1.0.1

--- includes/content/main/footer.inc.php
+++ includes/content/main/footer.inc.php
@@ -3,7 +3,7 @@
  	<div class="clearfix"></div>
  	</div>
         <div id="footer">
-            <a class="last" target="_blank" href="https://github.com/pataga/">&copy; <?php echo date('Y')?> SAS - Server Admin System [v1.0.0]</a>
+            <a class="last" target="_blank" href="https://github.com/pataga/">&copy; <?php echo date('Y')?> SAS - Server Admin System [v1.0.1]</a>
             <br><br> 
             <a onclick="alert(unescape('Diese Seite ist vor%FCbergehend nicht erreichbar.'))" href="#">Patrick Farnkopf</a> <a onclick="alert(unescape('Diese Seite ist vor%FCbergehend nicht erreichbar.'))" href="#">Tanja Weiser</a><a class="last" href="http://mangopix.de" target="_blank">Gabriel Wanzek</a>
         </div>
