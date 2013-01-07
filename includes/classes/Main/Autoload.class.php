50749ff888597d7816e91f256dfc3dd14a0fb167
Patrick Farnkopf <patrickfarnkopf@web.de>
2013-01-07 17:26:38 +0100

    Klassen/Struktur: Dateinamen abgeändert (für Umstieg auf Namespaces)

---
+++ includes/classes/Main/Autoload.class.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/**
+* Licensed under The Apache License
+*
+* @copyright Copyright 2012-2013 Patrick Farnkopf, Tanja Weiser, Gabriel Wanzek (PaTaGa)
+* @link https://github.com/pataga/SAS
+* @since SAS v1.0.0
+* @license Apache License v2 (http://www.apache.org/licenses/LICENSE-2.0.txt
+* @author Patrick Farnkopf
+*
+*/
+
+
+class AutoLoad {
+    public function __construct($class) {
+        try {
+            require '';
+        } catch (Exception $e) {
+            
+        }
+    }
+}
