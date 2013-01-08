4191299ba0701721b21f47cb4a2cb2b6d9908ba0
Patrick Farnkopf <patrickfarnkopf@web.de>
2013-01-08 18:08:44 +0100

    Klassen/Session: Füge Basis für die neue Klasse "Session" hinzu (noch keine Implementierung)

---
+++ includes/classes/Session.class.php
@@ -0,0 +1,146 @@
+<?php
+
+
+/**
+* Licensed under The Apache License
+*
+* @copyright Copyright 2012-2013 Patrick Farnkopf, Tanja Weiser, Gabriel Wanzek (PaTaGa)
+* @link https://github.com/pataga/SAS
+* @since SAS v1.0.0
+* @license Apache License v2 (http://www.apache.org/licenses/LICENSE-2.0.txt
+* @author Patrick Farnkopf
+*
+*/
+
+class Session {
+    /**
+     * Starte Session 
+     */
+    public function __construct() {
+        session_start();
+
+        //User Data Initialisierung
+        $_SESSION['user']['authenticated'] = false;
+        $_SESSION['user']['failedAuths'] = 0;
+        $_SESSION['user']['id'] = 0;
+        $_SESSION['user']['name'] = '';
+        $_SESSION['user']['email'] = '';
+        $_SESSION['user']['admin'] = 0;
+
+        //Server Data Initialisierung
+        $_SESSION['server']['chosen'] = false;
+        $_SESSION['server']['id'] = 0;
+        $_SESSION['server']['name'] = '';
+        $_SESSION['server']['address'] = '';
+
+        //Debug Data Initialisierung
+        $_SESSION['debug']['logLevel'] = 2; //Userspezifisches logLevel (für Entwicklung)
+        $_SESSION['debug']['errorsCount'] = 0;
+    }
+
+    /**
+     * Überprüft, ob der Benutzer sich authentifiziert hat
+     * @return bool 
+     */
+    public function IsLoggedIn() {
+        return isset($_SESSION['user']['authenticated']) && $_SESSION['user']['authenticated'];
+    }
+
+    /**
+     * Gibt einen boolschen Wert zurück, der angibt, ob ein Server ausgewählt wurde.
+     * @return bool
+     */
+    public function IsServerChosen() {
+        return $_SESSION['server']['chosen'];
+    }
+
+    /**
+     * Gibt die Datenbank ID des Servers zurück
+     * @return int id
+     */
+    public function GetServerID() {
+        return $_SESSION['server']['id'];
+    }
+
+    /**
+     * Gibt Server Namen zurück
+     * @return String name
+     */
+    public function GetServerName() {
+        return $_SESSION['server']['name'];
+    }
+
+    /**
+     * Gibt Server Adresse zurück
+     * @return String address
+     */
+    public function GetServerAddress() {
+        return $_SESSION['server']['address'];
+    }
+
+    /**
+     * Gibt den Auth Status des Benutzers zurück
+     * @return bool status
+     */
+    public function IsAuthenticated() {
+        return $_SESSION['user']['authenticated'];
+    }
+
+    /**
+     * Gibt zurück, wie oft mit den falschen Benutzerdaten ein Login versucht wurde.
+     * @return int count
+     */
+    public function GetFailedAuths() {
+        return $_SESSION['user']['failedAuths'];
+    }
+
+    /**
+     * Gibt die ID des Benutzers zurück
+     * @return int id
+     */
+    public function GetUserId() {
+        return $_SESSION['user']['id'];
+    }
+
+    /**
+     * Gibt den Benutzernamen zurück
+     * @return String username
+     */
+    public function GetUserName() {
+        return $_SESSION['user']['name'];
+    }
+
+    /**
+     * Gibt die Emailadresse des Benutzers zurück
+     * @return String email
+     */
+    public function GetUserEmail() {
+        return $_SESSION['user']['email'];
+    }
+
+    /**
+     * Gibt zurück ob der Benutzer ein Administrator ist
+     * @return bool admin
+     */
+    public function IsAdmin() {
+        return $_SESSION['user']['admin'];
+    }
+
+    /**
+     * Gibt die Anzahl der Fehler auf der aktuelle Seite zurück
+     * @return int count
+     */
+    public function GetErrorsCount() {
+        return $_SESSION['debug']['errorsCount'];
+    }
+
+    /**
+     * Gibt Userspezifisches logLevel zurück
+     * @return int logLevel
+     */
+    public function GetLogLevel() {
+        return $_SESSION['debug']['logLevel'];
+    }
+}
+
+?>
