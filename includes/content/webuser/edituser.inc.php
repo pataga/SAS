f38cec6f84b1d3d295afe4b9ee0ffb0da7b56473
Tanja Weiser <root@tanja-N55SF.(none)>
2013-01-21 13:15:54 +0100

    Webuser Unterseiten & Webuser bearbeiten Rechtevergabe Grundgerüst hinzugefgt

--- includes/content/webuser/edituser.inc.php
+++ includes/content/webuser/edituser.inc.php
@@ -32,15 +32,204 @@ if (isset($_POST['id'])) {
 
     echo '
 
+<h3>Benutzer bearbeiten</h3>
+<fieldset>
 <form action="index.php?p=webuser&s=edit" method="post">
-    Username:<input type="text" name="user" value="'.$row->username.'"><br>
-    E-Mail:<input type="text" name="mail" value="'.$row->email.'"><br>
-    Passwort:<input type="password" name="pw" id=""><br>
-    Passwort wiederholen:<input type="password" name="pwr" id=""><br>
-    <input type="submit" name="absenden" value="absenden"><br>
+    <p><label>Username:</label><input type="text" name="user" value="'.$row->username.'" class="text-long"></p>
+    <p><label>E-Mail:</label><input type="text" name="mail" value="'.$row->email.'" class="text-long"></p>
+    <p><label>Passwort:</label><input type="password" name="pw" id="" class="text-long"></p>
+    <p><label>Passwort wiederholen:</label><input type="password" name="pwr" id="" class="text-long"></p>
+    <input type="submit" name="absenden" value="absenden" class="button blue"><br>
+    <input type="hidden" name="id" value="'.$id.'">
+</fieldset>
+
+<fieldset>
+<legend>Rechtevergabe</legend>
+    <div class="halbe-box">
+<fieldset>
+    <legend>Home</legend>
+    <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div> 
+        <div class="drittel-box lastbox"> Verweigern  <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Quickpanel</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+</div>
+<div class="halbe-box lastbox">
+<fieldset>
+    <legend>MySQL</legend>
+    <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Benutzer anlegen</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Datenbanke</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+</div>
+<div class="halbe-box">
+<fieldset>
+    <legend>Postfix</legend>
+    <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Konfiguration</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Benutzer</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Statistik</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+</div>
+<div class="halbe-box lastbox">
+<fieldset>
+    <legend>Control</legend>
+    <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Paket Installation</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Selbstzerst&ouml;rung</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Neustarten</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+</div>
+<div class="halbe-box">
+<fieldset>
+<legend>FTP</legend>
+   <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Control</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Verzeichnisse</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Benutzer</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Statistik</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+</div>
+<div class="halbe-box lastbox">
+<fieldset>
+<legend>Samba</legend>
+    <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Konfiguration</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">freigaben</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Freigaben bearbeiten</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Benutzer</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+</div>
+<div class="halbe-box">
+<fieldset>
+    <legend>Apache</legend>
+    <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Konfiguration</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Control</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Hosting-System</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Module</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Statistik</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">phpinfo</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+
+<fieldset>
+<legend>Webuser</legend>
+    <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Benutzer hinzuf&uuml;gen</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Benutzer bearbeiten</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+
+
+<br><br>
+    <input type="submit" name="absenden" value="absenden" class="button blue"><br>
     <input type="hidden" name="id" value="'.$id.'">
 </form>
-';
+<br><br>
+
+
+</div>
+<div class="halbe-box lastbox">
+<fieldset>
+    <legend>Tools</legend>
+    <p> <div class="drittel-box">&Uuml;bersicht</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Konsole</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">CPU Auslastung</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Cronjobs</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Festplatten Informationen</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Hardware Informationen</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+    <p> <div class="drittel-box">Arbeitsspeicher Informationen</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+     <p> <div class="drittel-box">Taskmanager</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+     <p> <div class="drittel-box">Statistiken</div> <div class="drittel-box"> Erlauben <input type="checkbox" name="" id=""></div>
+        <div class="drittel-box lastbox"> Verweigern <input type="checkbox" name="" id=""></div>
+    </p>
+</fieldset>
+</div>
+
+
+
+</fieldset>
+
+        ';
 }
 
 if (isset($_POST['absenden'])) {
@@ -69,9 +258,14 @@ if (isset($_POST['edituser']))
 
 
 <!--############ User bearbeiten ############//-->
+
+<? if (!isset($_POST['id'])) {?>
 <h3>Benutzer bearbeiten</h3>
 <fieldset>
     <?=$content?>
 <form action="index.php?p=webuser&s=edit" method="POST">   
 </form>
 </fieldset>
+
+<?}?>
+
