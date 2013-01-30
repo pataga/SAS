0ecc21ec5a5854086e625719fc09889c174172b4
GabrielWanzek <gabrielwanzek@ymail.com>
2013-01-30 21:57:50 +0100

    css für radio/checkbox, XML-Menü fertig, div. Änderungen wegen Umstieg
    Signed-off-by: GabrielWanzek <gabrielwanzek@ymail.com>

--- includes/content/plugins/overview.inc.php
+++ includes/content/plugins/overview.inc.php
@@ -1,4 +1,17 @@
 <h3>SAS Plugins</h3>
+<fieldset>
+	<h5>Ideen für Plugins:</h5>
+	<ul class="square">
+		<li>Minecraft Install &amp; Control Plugin</li>
+		<li>lighttpd Plugin</li>
+		<li>vsftpd <i>oder</i> DrFTPD Plugin</li>
+		<li>CUPS Plugin</li>
+		<li>Ampache Plugin</li>
+		<li>PostgreSQL Plugin</li>
+		<li>CMS Verwaltungs und Installationspugin</li>
+		<li>DHCP-Server-Plugin</li>
+	</ul>
+</fieldset>
 <a href="#LEER"><div class="viertel-box boxitem"><p><i>leer</i></p></div></a>
 <a href="#LEER"><div class="viertel-box boxitem"><p><i>leer</i></p></div></a>
 <a href="#LEER"><div class="viertel-box boxitem"><p><i>leer</i></p></div></a>
--- includes/content/tools/overview.inc.php
+++ includes/content/tools/overview.inc.php
@@ -1,8 +1,8 @@
 <h3>SAS Tools</h3>
-<a href="index.php?p=tools&s=hdd"><div class="viertel-box boxitem"><p class="long">Festplatten- Informationen</p></div></a>
-<a href="index.php?p=tools&s=hw"><div class="viertel-box boxitem"><p class="long">Hardware- Informationen</p></div></a>
-<a href="index.php?p=tools&s=ram"><div class="viertel-box boxitem"><p class="long">Arbeitsspeicher- Informationen</p></div></a>
-<a href="index.php?p=tools&s=info"><div class="viertel-box boxitem lastbox"><p class="long">Sonstige- Informationen</p></div>
+<a href="index.php?p=tools&s=hddinfo"><div class="viertel-box boxitem"><p class="long">Festplatten- Informationen</p></div></a>
+<a href="index.php?p=tools&s=hwinfo"><div class="viertel-box boxitem"><p class="long">Hardware- Informationen</p></div></a>
+<a href="index.php?p=tools&s=ramload"><div class="viertel-box boxitem"><p class="long">Arbeitsspeicher- Informationen</p></div></a>
+<a href="index.php?p=tools&s=info"><div class="viertel-box boxitem lastbox"><p class="long">Sonstige Informationen</p></div>
 <div class="clearfix"></div>
 
 <a href="#LEER"><div class="viertel-box boxitem"><p><i>leer</i></p></div></a>
