b465eb23789b59e4c0606abde5a633aac8e54314
Patrick Farnkopf <patrickfarnkopf@web.de>
2013-02-04 17:13:52 +0100

    Classes: Beginne Umstrukturierung (neue MySQL Klasse + neue Ordnerstruktur)

---
+++ includes/Config/MySQL.conf.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Config;
+
+interface MySQL {
+    const HOST_ADDRESS    =   'localhost';
+    const USERNAME        =   'root';
+    const PASSWORD        =   '';
+    const DATABASE        =   'sas';
+}
+
+?>
---
+++ includes/Config/System.conf.php
@@ -0,0 +1,32 @@
+<?php
+namespace Config;
+
+interface System {
+    /****************************
+     * SAS System Konfiguration *
+     ****************************/
+
+
+
+    /**************************************************************
+     * Das SYSTEM_DEBUG_LEVEL gibt die Fehlerempfindlichkeit von SAS an
+     * 
+     * SYSTEM_DEBUG_LEVEL = 0 (Kein Debugging)
+     * SYSTEM_DEBUG_LEVEL = 1 (Fehler werden in eine Logdatei geschrieben)
+     * SYSTEM_DEBUG_LEVEL = 2 (Fehler werden in eine Logdatei geschrieben - Ladeabbruch bei Fatal Errors)
+     *
+     * Default: 2
+     **************************************************************/
+    const SYSTEM_DEBUG_LEVEL = 2;
+
+
+
+    /**************************************************************
+     * Mit SYSTEM_LOG_FILE kann bestimmt werden, in welcher Datei die Fehler gespeichert werden sollen
+     * 
+     * Default: './error.log'
+     **************************************************************/
+    const SYSTEM_LOG_FILE = './error.log';
+}
+
+?>
