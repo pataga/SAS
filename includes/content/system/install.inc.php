390a2c03ee1a957d29241bbaea7d349951879c82
Gabriel Wanzek <gabrielwanzek@ymail.com>
2013-02-05 18:46:53 +0100

    Update includes/content/system/install.inc.php
    format + fix

--- includes/content/system/install.inc.php
+++ includes/content/system/install.inc.php
@@ -1,23 +1,23 @@
 <h3>Paket Installation</h3>
 <form action="index.php?p=system&s=install" method="POST">
-<fieldset>	
-<legend>Paketsuche</legend>	
-<div class="halbe-box">
-<input type="text" class="text-long" name="aptcache">
-<br><br><br>
-<input type="submit" class="button green" name="search" value="suchen">
-<br>
-</div>
-<div class ="halbe-box lastbox">
-Gefundene Pakete:
-</div>
-</fieldset>
-<fieldset>
-<legend>Installation</legend>
-<label>Paketname:</label>
-<input type="text" class="text-long" name="aptgetinstall">
-<br><br><br>
-<input type="submit" class="button green" name="install" value="installieren">
-</fieldset>
+    <fieldset>  
+        <legend>Paketsuche</legend>	
+        <div class="halbe-box">
+            <input type="text" class="text-long" name="aptcache">
+            <br><br><br>
+            <input type="submit" class="button green" name="search" value="suchen">
+            <br>
+        </div>
+        <div class="halbe-box lastbox">
+            Gefundene Pakete:
+        </div>
+    </fieldset>
+    <fieldset>
+        <legend>Installation</legend>
+        <label>Paketname:</label>
+        <input type="text" class="text-long" name="aptgetinstall">
+        <br><br><br>
+        <input type="submit" class="button green" name="install" value="installieren">
+    </fieldset>
 </from>
 
