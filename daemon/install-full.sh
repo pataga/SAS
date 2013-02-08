dced179dfef76833bbb0c770ce6323e1bc20184a
Patrick Farnkopf <patrickfarnkopf@web.de>
2013-02-09 00:28:22 +0100

    Daemon: Install fixed + cleanup

--- daemon/install-full.sh
+++ daemon/install-full.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+out=`whoami`
+if [ "$out" != "root" ]; then
+    echo "Der Installer muss als Root ausgefuehrt werden!"
+    exit
+fi
 echo "#############################"
 echo "#### Server Admin System ####"
 echo "##### Daemon Installer ######"
@@ -6,11 +11,13 @@ echo "#############################"
 echo -e "\n"
 read -p "Installation starten mit beliebiger Taste" -n1
 echo -e "\n\n"
+echo "Installation wurde gestartet. Dies kann einige Zeit in Anspruch nehmen"
 rm -rf /usr/bin/SASDaemon
 rm -rf /usr/lib/SASDaemon
 rm -rf /var/run/SASDaemon
 rm -rf /etc/init.d/SASDaemon
 rm -rf /etc/SASDaemon/
+cd contrib && tar xfv daemon.tar > /dev/null && cd daemon && chmod a+x setup.rb && ./setup.rb > /dev/null
 apt-get install ruby1.8-full -fy > /dev/null
 apt-get install rubygems -fy > /dev/null
 rm -rf /SASDaemon.tar > /dev/null
@@ -18,7 +25,6 @@ cp SASDaemon.tar / -f > /dev/null
 cd / && tar xfv SASDaemon.tar > /dev/null
 chmod 770 /usr/bin/SASDaemon > /dev/null
 rm -rf /SASDaemon.tar > /dev/null
-cd contrib && tar xfv daemon.tar > /dev/null && cd daemon && chmod a+x setup.rb && ./setup.rb > /dev/null
 chmod a+x /etc/init.d/SASDaemon
 
 echo -e "\nSASDaemon wurde erfolgreich installiert und kann nun mittels service SASDaemon start gestartet werden"
