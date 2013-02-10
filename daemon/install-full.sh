612f29163fc0fc682aed065247a4325ffc399feb
Patrick Farnkopf <patrickfarnkopf@web.de>
2013-02-10 11:43:22 +0100

    Daemon: Einlesen der Config wurde gefixed. Konfigurationsmanager wurde im Full Installer hinzugefügt

--- daemon/install-full.sh
+++ daemon/install-full.sh
@@ -27,5 +27,15 @@ chmod 770 /usr/bin/SASDaemon > /dev/null
 rm -rf /SASDaemon.tar > /dev/null
 chmod a+x /etc/init.d/SASDaemon
 
-echo -e "\nSASDaemon wurde erfolgreich installiert und kann nun mittels service SASDaemon start gestartet werden"
+read -p "\n\n\nZum Aufrufen des SASDaemon Konfigurationsmanagers die Taste K druecken" -n1 opt
+if [ "$opt" = "k" ]; then
+    read -p "Wie lautet die Adresse von SAS? " host
+    read -p "Welchen Port soll der Daemon verwenden? " port 
+    key=`openssl rand -base64 32`
+    echo "host=$host" > /etc/SASDaemon/SASd.conf
+    echo "port=$port" >> /etc/SASDaemon/SASd.conf
+    echo "debug=false" >> /etc/SASDaemon/SASd.conf
+    echo "key=$key" >> /etc/SASDaemon/SASd.conf
+fi 
 
+echo -e "\nSASDaemon wurde erfolgreich installiert und kann nun mittels 'service SASDaemon start' gestartet werden\n"
