e166913303c76b5c7d7271f7d3c986a5a56bf741
root <root@kaki.(none)>
2013-03-15 03:25:43 +0100

    permission denied inc added

---
+++ includes/Content/error/permisson.inc.php
@@ -0,0 +1,11 @@
+<br>
+<fieldset>
+    <img src="img/error.png" alt="404" style="float:left; padding-right:25px;">
+    <h1>Zugriff verweigert!</h1>
+    <span>
+    	<b>Dieser Account verfügt nicht über die nötigen Rechte, um auf dieses Modul zuzugreifen.</b><br>
+    	<br>
+    	Wenden Sie sich im Zweifelsfall an den zuständigen Administrator.
+    </span>
+    <div class="clearfix"></div>
+</fieldset>
