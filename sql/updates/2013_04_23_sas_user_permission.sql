e0ff0a34e8f18e85eff282081139c865cd0fb2c4
Patrick Farnkopf <patrickfarnkopf@web.de>
2013-04-25 15:59:04 +0200

    SQL: Vergaß DB Update
    In dieser Datei befinden sich Anweisungen zum vergeben voller Berechtigungen.
    Einfach die Werte von @SERVER_ID und @USER_ID nach Bedarf anpassen

--- sql/updates/2013_04_23_sas_user_permission.sql
+++ sql/updates/2013_04_23_sas_user_permission.sql
@@ -1,8 +1,24 @@
 DROP TABLE IF EXISTS `sas_user_permission`;
 CREATE TABLE `sas_user_permission` (
+  `pid` int(11) unsigned NOT NULL,
+  `sid` int(11) unsigned NOT NULL,
   `uid` int(11) unsigned NOT NULL,
   `bitmask` bigint(20) NOT NULL,
-  PRIMARY KEY (`uid`)
+  PRIMARY KEY (`pid`,`sid`,`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `sas_user_permission` SELECT id, 0xFFFFF FROM `sas_users`;
+-- KONFIGURATION START --
+SET @SERVER_ID = 1;
+SET @USER_ID = 1;
+-- KONFIGURATION ENDE --
+
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (1, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (2, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (3, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (4, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (5, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (6, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (7, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (8, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (9, @SERVER_ID, @USER_ID, 255);
+INSERT INTO sas_user_permission (pid, sid, uid, bitmask) VALUE (10, @SERVER_ID, @USER_ID, 255);
