b03a381e3532eb31878d9198d54876d6d00798f0
GabrielWanzek <gabrielwanzek@ymail.com>
2013-04-30 16:50:33 +0200

    Wenn man nun versucht das includes/ Verzeichnis oder dessen Unterverzeichnisse aufzurufen, wird man auf eine Fehlerseite umgeleitet
    Somit können INC-Dateien nur noch mit dem absoluten pfad aufgerufen werden, was jedoch nicht "behoben" wird
    
    Die Fehlerseite entspricht natürlich dem Style von SAS :smile:

---
+++ includes/Plugins/Content/HelloWorld/HelloWorld.plugin.php
@@ -0,0 +1,4 @@
+<fieldset>
+    <legend>SAS-HelloWorld-Plugin</legend>
+    <h1>Hallo Welt!</h1>
+</fieldset>
---
+++ includes/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Error</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <style>
+    #main { text-align: left; font-size: 11pt}
+    </style>
+</head>
+<body>
+    <div style="text-algin:center; width: 940px; margin: 0 auto; padding-top:20px;">
+        <h1 style="color:#222;margin-top:-5px;text-align:left;font-family:'Lobster',Arial,Helvetica,sans-serif;text-shadow:1px 1px #ccc;font-size:46px;font-weight:400;">Server <span style="color:rgba(11,117,178,1.0);text-shadow:1px 1px #333333;">Admin</span> System</h1>
+    </div>
+
+    <div class="clearfix"></div>
+    <div id="main">
+        <fieldset>
+            <div class="fuenftel-box boxcenter">
+                <img src="../img/error.png" style="width:100px;height:100px;padding-right:15px;">
+            </div>
+            <div class="vierfuenftel-box lastbox">
+                <b style="font-size:16pt;">Fehler!</b>
+                <br><br>
+                Sie haben keine Berechtigung um dieses Verzeichnis zu sehen.
+                <br><br>
+                <a href="../">zurück zu SAS</a>
+            </div>                
+        </fieldset>
+    </div>
+</body>
+</html>
